c1f4c08ca90772a8d6fd8323e458972f5640dd1b
HamletShu <ahngo13@naver.com>
2021-03-27 08:08:15 +0900

    example ListView ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,35 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    final items = List.generate(100, (i)=>i).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('제목'),
       ),
       body:
 
-      SingleChildScrollView(
-        child: ListBody(
-          children: items.map((i) => Text('$i')).toList(),
-        )
+      ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            trailing: Icon(Icons.navigate_next),
+            onTap:(){}
+          ),
+          ListTile(
+              leading: Icon(Icons.event),
+              title: Text('Event'),
+              trailing: Icon(Icons.navigate_next),
+              onTap:(){}
+          ),
+          ListTile(
+              leading: Icon(Icons.camera),
+              title: Text('Camera'),
+              trailing: Icon(Icons.navigate_next),
+              onTap:(){}
+          )
+        ],
       )
     );
   }
